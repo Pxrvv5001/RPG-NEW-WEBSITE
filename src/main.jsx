9fd989f4c25b371229c226150361cdf0bccc9468
Pxrvv5001 <codewithparv1@gmail.com>
2025-12-23 19:39:14 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom' // Check this import
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* The App must be inside BrowserRouter */}
         <BrowserRouter>
+            <ScrollToTop />
             <App />
         </BrowserRouter>
     </React.StrictMode>,
