1510a0c4864ea9b4eea7d65dd34155016d000c25
Pxrvv5001 <codewithparv1@gmail.com>
2025-12-24 21:46:27 +0530

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' // Check this import
-import ScrollToTop from './components/ScrollToTop.jsx'
+import { HashRouter } from 'react-router-dom' // Change this import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* The App must be inside BrowserRouter */}
-        <BrowserRouter>
+        {/* Swap BrowserRouter for HashRouter */}
+        <HashRouter>
             <ScrollToTop />
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
